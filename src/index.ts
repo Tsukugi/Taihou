fe1ba32132778d0605a249844ffbee90d919911f
Tsukugi <TsukugiXIII@gmail.com>
2021-10-26 21:59:41 +0200

    Version 0.1.4
    Export types to construct nice Stores

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { createSection } from './store/createSection';
 export { createStore } from './store/createStore';
+export { Section, Commit, Process, DispatchCommit, DispatchProcess } from './interfaces';
